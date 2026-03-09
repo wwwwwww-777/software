1a36f1de8fbd3857427fd6a82954a45202b1ed03
wwwwwww-777 <2024302111390@whu.edu.cn>
2026-03-09 13:24:57 +0800

    Add files via upload

---
+++ one/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("你好！我是王诗雨！");
+
+        }
+    }
+}
